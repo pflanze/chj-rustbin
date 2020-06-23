7c6f3cbb7715d3fbc9c10a61a9c9bb4ca0fcd224
Christian Jaeger <ch@christianjaeger.ch>
2020-06-23 23:22:12 +0100

    Use just a single Result scope

--- src/symlinks-index.rs
+++ src/symlinks-index.rs
@@ -216,28 +216,27 @@ fn main() {
     let output_separator = if opt_0 || opt_z { 0 } else { b'\n' };
 
     if let Some(mut dirpaths) = args.values_of("directory-path") {
-        match dirs_index(debug, &mut dirpaths, remove_base) {
-            Ok(items_from_target) => {
-                if debug {
-                    eprintln!("items_from_target = {:?}", &items_from_target)
-                };
-                match serve(
-                    debug,
-                    stdin(),
-                    stdout(),
-                    items_from_target,
-                    input_separator,
-                    output_separator,
-                ) {
-                    Ok(()) => (),
-                    Err(err) => {
-                        eprintln!("Error serving pipe: {:#}", err);
-                        exit(1);
-                    }
-                }
+        match (|| -> Result<()> {
+            let items_from_target =
+                dirs_index(debug, &mut dirpaths, remove_base)
+                    .with_context(|| "indexing")?;
+            if debug {
+                eprintln!("items_from_target = {:?}", &items_from_target)
             }
+            serve(
+                debug,
+                stdin(),
+                stdout(),
+                items_from_target,
+                input_separator,
+                output_separator,
+            )
+            .with_context(|| "serving pipe")?;
+            Ok(())
+        })() {
+            Ok(()) => (),
             Err(err) => {
-                eprintln!("Error indexing: {:#}", err);
+                eprintln!("Error {:#}", err);
                 exit(1);
             }
         }

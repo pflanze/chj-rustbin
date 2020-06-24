e2de14d40ecb06dde1679c59a91394f26cc73095
Christian Jaeger <ch@christianjaeger.ch>
2020-06-24 21:35:54 +0100

    Use structopt instead of clap

--- src/symlinks-index.rs
+++ src/symlinks-index.rs
@@ -1,9 +1,12 @@
 // Same functionality as symlinks-index from
 // https://github.com/pflanze/chj-bin/blob/master/symlinks-index
 
-extern crate clap;
+#[macro_use]
+extern crate log;
+extern crate stderrlog;
+
+use structopt::StructOpt;
 use anyhow::{Context, Result};
-use clap::{App, Arg};
 use std::collections::HashMap;
 use std::ffi::{OsStr, OsString};
 use std::fs;
@@ -13,9 +16,6 @@ use std::io::{
 use std::os::unix::ffi::{OsStrExt, OsStringExt};
 use std::path::PathBuf;
 use std::process::exit;
-#[macro_use]
-extern crate log;
-extern crate stderrlog;
 
 macro_rules! Do {
     ( $($b:tt)* ) => ( (|| -> Result<_> { $($b)* })() )
@@ -28,7 +28,7 @@ fn cleanup_target(path: PathBuf) -> PathBuf {
 }
 
 fn dirs_index(
-    dirs: &mut dyn Iterator<Item = &str>,
+    dirs: &Vec<PathBuf>,
     remove_base: Option<OsString>,
 ) -> Result<HashMap<PathBuf, Vec<OsString>>> {
     let mut items_from_target: HashMap<PathBuf, Vec<OsString>> = HashMap::new();
@@ -148,78 +148,62 @@ impl StartsWith<&OsString> for OsStr {
     }
 }
 
+
+#[derive(StructOpt, Debug)]
+/// 
+/// On startup creates an in-memory index of the symlinks 
+/// contained in specified directories, then repeatedly 
+/// reads a target-path from stdin and writes the source-paths 
+/// that link to the target-path to stdout. The end of an answer 
+/// group is indicated by an empty string. Entries are 
+/// terminated by newline characters by default, but see -z 
+/// and -0.
+/// 
+/// See the `coproc` feature in Bash for how to integrate this 
+/// into scripts.
+#[structopt(name = "symlinks-index")]
+struct Opt {
+    /// show debugging output
+    #[structopt(short, long, parse(from_occurrences))]
+    debug: u8,
+
+    /// use the null byte as record terminator for writing
+    #[structopt(short, long)]
+    z: bool,
+
+    /// use the null byte as record terminator for reading 
+    /// and writing
+    #[structopt(long)]
+    zz: bool,
+
+    /// remove base from the target paths before putting
+    /// them into the index (simply substring it (for now))
+    #[structopt(long, parse(from_os_str))]
+    remove_base: Option<PathBuf>,
+
+    /// paths to one or more directories to be scanned for symlinks
+    /// to index
+    #[structopt(name= "DIR", parse(from_os_str), required(true))]
+    directory_paths: Vec<PathBuf>,
+}
+
 fn main() {
-    let app = App::new("symlinks-index")
-        // .author("Christian Jaeger") looks messy
-        .about(
-            "On startup creates an in-memory index of the symlinks \
-             contained in specified directories, then repeatedly \
-             reads a target-path from stdin and writes the source-paths \
-             that link to the target-path to stdout. The end of an answer \
-             group is indicated by an empty string. Entries are \
-             terminated by newline characters by default, but see -z \
-             and -0. \n\
-             \n\
-             See the `coproc` feature in Bash for how to integrate this \
-             into scripts.",
-        )
-        .arg(
-            Arg::with_name("debug")
-                .short("d")
-                .long("debug")
-                .multiple(true)
-                .help("show debugging output"),
-        )
-        .arg(
-            Arg::with_name("null as write terminator")
-                .short("z")
-                .help("use the null byte as record terminator for writing"),
-        )
-        .arg(
-            Arg::with_name("null as read+write terminator")
-                .short("0")
-                .help(
-                    "use the null byte as record terminator for reading \
-                     and writing",
-                ),
-        )
-        .arg(
-            Arg::with_name("remove base")
-                .long("remove-base")
-                .help(
-                    "remove base from the target paths before putting \
-                     them into the index (simply substring it (for now))",
-                )
-                .value_name("PATH"),
-        )
-        .arg(Arg::with_name("directory-path").multiple(true).help(
-            "paths to one or more directories to be scanned for symlinks \
-             to index",
-        ));
+    let opt = Opt::from_args();
 
-    let args = app.get_matches();
-
-    let debug = args.is_present("debug");
-    let remove_base = args
-        .value_of("remove base")
-        .and_then(|s| Some(OsString::from(s)));
-    let opt_z = args.is_present("null as write terminator");
-    let opt_0 = args.is_present("null as read+write terminator");
-    let input_separator = if opt_0 { 0 } else { b'\n' };
-    let output_separator = if opt_0 || opt_z { 0 } else { b'\n' };
+    let debug = opt.debug;
+    let remove_base = opt.remove_base.and_then(|s| Some(OsString::from(s)));
+    let input_separator = if opt.zz { 0 } else { b'\n' };
+    let output_separator = if opt.zz || opt.z { 0 } else { b'\n' };
+    let dirpaths = opt.directory_paths;
 
     (Do! {
         stderrlog::new()
             .module(module_path!())
-            .verbosity(if debug { 5 } else { 0 })
+            .verbosity(if debug == 0 { 0 } else { 5 })
             .init()?;
 
-        let mut dirpaths = args.values_of("directory-path")
-            .with_context(|| "parsing arguments: missing directory-path. \
-                              Run with --help for help.")?;
-
         let items_from_target =
-            dirs_index(&mut dirpaths, remove_base)
+            dirs_index(&dirpaths, remove_base)
                 .with_context(|| "indexing")?;
 
         trace!("items_from_target = {:?}", &items_from_target);

558edd17dccfab86f7ff2a4ea22635cca57e99fd
Christian Jaeger <ch@christianjaeger.ch>
2022-09-26 01:07:34 +0100

    Revert "lastitem: avoid a heap allocation?"
    This reverts commit 4e9346bab6285c40952ff6ded25ec3d11395770d.

--- src/bin/lastitem.rs
+++ src/bin/lastitem.rs
@@ -139,14 +139,14 @@ fn main() -> Result<()> {
 
     match newest_item {
         Some(Item { filename, mtime: _ }) => {
-            let mut p = PathBuf::new();
             io::stdout().write_all(
                 if opt.fullpath {
+                    let mut p = PathBuf::new();
                     p.push(&opt.directory_path);
                     p.push(filename);
-                    p.as_os_str()
+                    p.into_os_string()
                 } else {
-                    &filename
+                    filename
                 }.as_bytes())?;
             io::stdout().write_all(
                 "\n".as_bytes())

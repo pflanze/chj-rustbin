aba6893106cb8a81f4a6405305bdc23bd5e8bfc3
Christian Jaeger <ch@christianjaeger.ch>
2022-12-29 19:41:43 +0100

    _e-gnu: rework into _e, subsuming _e-gnu-multiarg
    - Rename Slurp256ParseError to Slurp256Error (was planning to use it
      for more than parse, but ended up not to).
    
    - Add argument parsing logic from the _e-gnu-multiarg Perl script, as
      well as its child forking and waiting approach, and an
      implementation of `ask_yn`.
    
    - Use an approach with a closure for the work in the daemon child (a
      different one depending on the arguments).
    
    - Factor out `run_cmd_with_log` (from the daemon child).
    
    - Test whether emacs is up via a separate evaluation if necessary
      (multiple arguments). Use slurp256_parse to parse the result of the
      evaluation, adding trailing newline stripping option, and change
      back to a parametrized result type.
    
    - Add `fork_cmd` and `backtick`, as well as `xcheck_exit_success`
    
    Remaining problems:
    
    - emacsclient issues "Waiting for Emacs..." from individual fork_cmd
      that isn't filtered anymore now.

--- src/bin/_e-gnu.rs
+++ src/bin/_e-gnu.rs
@@ -7,7 +7,7 @@ mod rawfdreader;
 use rawfdreader::RawFdReader;
 use anyhow::{Result, anyhow, bail}; 
 use std::{env, writeln};
-use std::io::{Write, BufReader, BufRead};
+use std::io::{stdin, Write, BufReader, BufRead};
 use libc::_exit;
 use nix::unistd::{getpid, pipe, fork, ForkResult,
                   close, setsid, dup2, execvp, read, write};
@@ -17,13 +17,15 @@ use nix::fcntl::{open, OFlag};
 use nix::sys::stat::{mode_t, Mode};
 use nix::sys::wait::{waitpid, WaitStatus};
 use std::os::unix::io::{FromRawFd, RawFd};
-use std::ffi::{CString, OsString};
+use std::ffi::{CString, OsString, OsStr};
 use std::os::unix::ffi::{OsStringExt};
 use nix::sys::signal::Signal;
-use bstr_parse::{BStrParse, ParseIntError};
+use bstr_parse::{BStrParse, ParseIntError, FromBStr};
 use nix::errno::Errno;
 use thiserror::Error;
 use nix::unistd::Pid;
+//use nix::sys::wait::Id::Pid;
+use std::process::exit;
 
 
 // There's no try_map, so:
@@ -116,39 +118,291 @@ unsafe fn easy_fork() -> Result<Option<Pid>> {
     }
 }
 
+// XX replace with vfork/exec or rather posix_spawnp
+unsafe fn fork_cmd(cmd: &[CString]) -> Result<Pid> {
+    if let Some(pid) = easy_fork()? {
+        Ok(pid)
+    } else {
+        execvp(&cmd[0], &cmd)?;
+        Ok(Pid::from_raw(0)) // never reached, satisfy type system
+    }
+}
+
+
+fn xcheck_exit_success(res: Result<i32>, cmd: &[CString]) -> Result<()> {
+    let exitcode = res?;
+    if exitcode == 0 {
+        Ok(())
+    } else {
+        bail!("command ended with error exit code {}: {:?}",
+              exitcode, cmd)
+    }
+}
+
+fn ask_yn(question: &str) -> Result<bool> {
+    for n in (1..5).rev() {
+        println!("{} (y/n)", question);
+        let mut ans = String::new();
+        stdin().read_line(&mut ans)?;
+        if ans.len() > 1 && ans.starts_with("y") {
+            return Ok(true)
+        } else if ans.len() > 1 && ans.starts_with("n") {
+            return Ok(false)
+        }
+        println!("please answer with y or n, {} tries left", n);
+    }
+    bail!("could not get an answer to the question {:?}",
+          question)
+}
+
+
 #[derive(Error, Debug)]
-enum Slurp256ParseError {
+enum Slurp256Error {
     #[error("I/O error: {0}")]
     Io(Errno),
     #[error("input is too large")]
     InputTooLarge,
     #[error("parse error: {0} for input: {1:?}")]
-    Pie(ParseIntError, Vec<u8>)
+    NoParse(ParseIntError, Vec<u8>),
 }
 
-fn slurp256_parse(fd: RawFd) -> Result<i32, Slurp256ParseError> {
+fn slurp256_parse<T: FromBStr<Err = bstr_parse::ParseIntError>>(
+    fd: RawFd,
+    do_chomp: bool,
+) -> Result<T, Slurp256Error> {
     let mut buf : [u8; 257] = [0; 257];
-    let len = read(fd, &mut buf).map_err(Slurp256ParseError::Io)?;
+    let len = read(fd, &mut buf).map_err(Slurp256Error::Io)?;
+    close(fd).or_else(|e| Err(Slurp256Error::Io(e)))?;
     if len == 257 {
-        Err(Slurp256ParseError::InputTooLarge)
+        return Err(Slurp256Error::InputTooLarge)
+    }
+    let end =
+        if do_chomp && len > 0 {
+            (|| {
+                for i in (0..len-1).rev() {
+                    if buf[i] != b'\n' {
+                        return i+1
+                    }
+                }
+                0
+            })()
+        } else {
+            len
+        };
+    let s = &buf[0..end];
+    s.parse().or_else(
+        |e| Err(Slurp256Error::NoParse(e, Vec::from(s))))
+}
+
+
+fn backtick<T: 'static + Send + Sync + std::fmt::Debug + std::fmt::Display
+            + FromBStr<Err = bstr_parse::ParseIntError>>(
+    cmd: &Vec<CString>,
+    do_chomp: bool,
+) -> Result<T> {
+    let (streamr, streamw) = pipe()?;
+    if let Some(pid) = unsafe { easy_fork() }? {
+        close(streamw)?;
+        let x = slurp256_parse(streamr, do_chomp)?;
+        xwaitpid_until_gone(pid)?;
+        Ok(x)
     } else {
-        buf[0..len].parse().or_else(
-            |e| Err(Slurp256ParseError::Pie(e, Vec::from(&buf[0..len]))))
+        close(streamr)?;
+        dup2(streamw, 1)?;
+        // dup2(streamw, 2)?;
+        close(streamw)?;
+
+        execvp(&cmd[0], &cmd)?;
+        unsafe { _exit(123) }; // never reached, to satisfy type system
     }
 }
 
+// Run cmd, waiting for its exit and logging its output.
+fn run_cmd_with_log(cmd: &Vec<CString>, logpath: &OsStr) -> Result<i32> {
+    let (streamr, streamw) = pipe()?;
+    if let Some(pid) = unsafe { easy_fork() }? {
+        close(streamw)?;
+        {
+            // XX does RawFd have a drop that closes? Should it?
+            let log : RawFd = open(
+                logpath,
+                OFlag::O_CREAT |
+                OFlag::O_WRONLY |
+                OFlag::O_APPEND,
+                mode_from_bits(0o600)?)?;
+            let reader = BufReader::new(
+                unsafe { RawFdReader::from_raw_fd(streamr) });
+            let mut have_written = false;
+            for line in reader.lines() {
+                let line = line?;
+                let line = string_remove_start(
+                    // emacsclient *always* prints this (to
+                    // indicate that the buffer needs to be
+                    // closed)
+                    &line, "Waiting for Emacs...");
+                if line.len() > 0 {
+                    let mut buf = Vec::new();
+                    writeln!(&mut buf, "{}\t({})\t{}",
+                             time()?, getpid(), line)?;
+                    write_all(log, &buf)?;
+                    if !have_written {
+                        eprintln!("starting Emacs instance");
+                        have_written = true;
+                    }
+                }
+            }
+            close(streamr)?;
+            close(log)?;
+        }
+
+        let status = waitpid_until_gone(pid)?;
+        // What's the best exit code to report a signal?
+        let exitcode =
+            if let Status::Normalexit(code) = status {
+                code
+            } else {
+                13
+            };
+        Ok(exitcode)
+    } else {
+        close(streamr)?;
+        // close(sigw)?; -- XX should close that, where?
+        dup2(streamw, 1)?;
+        dup2(streamw, 2)?;
+        close(streamw)?;
+
+        execvp(&cmd[0], &cmd)?;
+        Ok(0) // in child, never reached, just to satisfy type system
+    }
+}
+
+
 fn main() -> Result<()> {
 
-    let alternate_editor = env::var_os("EMACS_ALTERNATE_EDITOR")
-        .unwrap_or(OsString::from(""));
-    // println!("alternate_editor={:?}", alternate_editor);
-
+    let (args, args_is_all_files) = (|| -> Result<(Vec<CString>, bool)> {
+        let args = cstrings_from_osstrings(&mut env::args_os().skip(1))?;
+        let mut files : Vec<CString> = Vec::new();
+        let mut seen_boundary = false; // seen "--"
+        for arg in &args {
+            let a = arg.to_bytes();
+            if a == b"--" {
+                seen_boundary = true;
+            } else if ! seen_boundary && a.starts_with(b"-") {
+                println!("can't currently deal with options, falling \
+                          back to single emacsclient call (not opening \
+                          a separate frame per file)");
+                return Ok((args, false))
+            } else if a.starts_with(b"+") {
+                println!("can't currently deal with positions, falling \
+                          back to single emacsclient call (not opening \
+                          a separate frame per file)");
+                return Ok((args, false))
+            } else {
+                files.push(arg.clone());
+            }
+        }
+        Ok((files, true))
+    })()?;
+    
     let logpath = {
         let mut home = env::var_os("HOME").ok_or_else(
             || anyhow!("missing HOME var"))?;
         home.push("/._e-gnu_rs.log");
         home
     };
+
+    let daemonwork : Box<dyn FnOnce(&OsStr) -> Result<i32>> =
+        if !args_is_all_files || args.len() == 1 {
+            // Let emacsclient start the daemon on its own if
+            // necessary. That way we need to run just one command.
+
+            // XX What do we do with this env var?:
+            // let alternate_editor = env::var_os("ALTERNATE_EDITOR")
+            //     .unwrap_or(OsString::from(""));
+            // println!("alternate_editor={:?}", alternate_editor);
+
+            let mut cmd = vec!(
+                CString::new("emacsclient")?,
+                CString::new("-c")?,
+                {
+                    let alt = OsString::from("--alternate-editor=");
+                    // alt.push(alternate_editor);
+                    CString::new(alt.into_vec())?
+                }
+            );
+            cmd.append(&mut args.clone());
+
+            Box::new(move |logpath| run_cmd_with_log(&cmd, logpath))
+
+        } else {
+            let files = args;
+            if files.len() > 8 {
+                if ! ask_yn(&format!("got {} arguments, do you really want to open so many windows?",
+                                     files.len()))? {
+                    println!("cancelling");
+                    return Ok(());
+                }
+            }
+
+            Box::new(|logpath| {
+                // Check if emacs daemon is up, if not, start it. Then
+                // open each file (args is just files here) with a
+                // separate emacsclient call, so that each is opened in a
+                // separate frame.
+
+                let start_emacs = || -> Result<()> {
+                    let cmd = vec!(CString::new("emacs")?,
+                                   CString::new("--daemon")?);
+                    xcheck_exit_success(
+                        run_cmd_with_log(&cmd,
+                                         logpath),
+                        &cmd)?;
+                    Ok(())
+                };
+
+                let res : Result<i32> = backtick(
+                    &vec!(CString::new("emacsclient")?,
+                          CString::new("-e")?,
+                          CString::new("(+ 3 2)")?),
+                    true);
+                // println!("res= {:?}", res);
+                match res {
+                    Err(_) => {
+                        start_emacs()?
+                    },
+                    Ok(val) => {
+                        if val == 5 {
+                            // Emacs is already up
+                        } else {
+                            start_emacs()?
+                        }
+                    }
+                }
+
+                // Open each file separately, collecting the pids that
+                // we then wait on.
+                let mut pids = Vec::new();
+                for file in files {
+                    let cmd = vec!(
+                        CString::new("emacsclient")?,
+                        CString::new("-c")?,
+                        file);
+                    let pid = unsafe { fork_cmd(&cmd) }?;
+                    pids.push(pid);
+                }
+                // Collecting them out of their exit order. Only
+                // matters for early termination in case of errors
+                // (and to avoid zombies). Does anyone care?
+                for pid in pids {
+                    xwaitpid_until_gone(pid)?;
+                }
+                Ok(0)
+            })
+        };
+
+
+    // Run the emacs interfacing code in a child that's protected from
+    // ctl-c ("daemon").
 
     let (sigr, sigw) = pipe()?;
 
@@ -159,7 +413,7 @@ fn main() -> Result<()> {
         // block until buffer is closed, or more precisely, emacsclient is
         // finished, and receive its status:
 
-        let exitcode : i32 = slurp256_parse(sigr)?;
+        let exitcode : i32 = slurp256_parse(sigr, false)?;
         
         // my $statuscode= $1+0;
         // my $exitcode= $statuscode >> 8;
@@ -171,87 +425,22 @@ fn main() -> Result<()> {
         // } else {
         //     exit $exitcode;
         // }
-        unsafe { _exit(exitcode) };
+        exit(exitcode);
     } else {
         close(sigr)?;
         setsid()?; // prevent signals from crossing over (stop ctl-c)
-        let (streamr, streamw) = pipe()?;
-        if let Some(pid) = unsafe { easy_fork() }? {
-            close(streamw)?;
-            {
-                // XX does RawFd have a drop that closes? Should it?
-                let log : RawFd = open(
-                    logpath.as_os_str(),
-                    OFlag::O_CREAT |
-                    OFlag::O_WRONLY |
-                    OFlag::O_APPEND,
-                    mode_from_bits(0o600)?)?;
-                let reader = BufReader::new(
-                    unsafe { RawFdReader::from_raw_fd(streamr) });
-                let mut have_written = false;
-                for line in reader.lines() {
-                    let line = line?;
-                    let line = string_remove_start(
-                        // emacsclient *always* prints this (to
-                        // indicate that the buffer needs to be
-                        // closed)
-                        &line, "Waiting for Emacs...");
-                    if line.len() > 0 {
-                        let mut buf = Vec::new();
-                        writeln!(&mut buf, "{}\t({})\t{}",
-                                 time()?, getpid(), line)?;
-                        write_all(log, &buf)?;
-                        if !have_written {
-                            eprintln!("starting Emacs instance");
-                            have_written = true;
-                        }
-                    }
-                }
-	        close(streamr)?;
-                close(log)?;
-            }
-            
-            let status = waitpid_until_gone(pid)?;
-            // What's the best exit code to report a signal?
-            let exitcode = if let Status::Normalexit(code) = status {
-                code
-            } else {
-                13
-            };
-            let mut buf = Vec::new();
-            write!(&mut buf, "{}", exitcode)?;
-            // Ignore PIPE errors (in case the front process was
-            // killed by user; the Perl version was simply killed by
-            // SIGPIPE before it had a chance to print the error
-            // message about not being able to print to sigw):
-            let _ = write_all(sigw, &buf);
-            let _ = close(sigw);
-            unsafe { _exit(0) };
-        } else {
-	    close(streamr)?;
-	    close(sigw)?;
-	    dup2(streamw, 1)?;
-	    dup2(streamw, 2)?;
-            close(streamw)?;
 
-            let cmd = {
-                // The `--alternate-editor=` option is required for
-                // starting a new instance (if alternate_editor is
-                // empty, emacsclient uses "emacs --daemon" according
-                // to its docs).
-                let mut c = OsString::from("--alternate-editor=");
-                c.push(alternate_editor);
-                let mut cmd = vec!(
-                    CString::new("emacsclient")?,
-                    CString::new("-c")?,
-                    CString::new(c.into_vec())?);
-                cmd.append(&mut cstrings_from_osstrings(
-                    &mut env::args_os().skip(1))?);
-                cmd
-            };
-	    execvp(&cmd[0], &cmd)?;
-        }
+        let exitcode = daemonwork(&logpath)?;
+
+        let mut buf = Vec::new();
+        write!(&mut buf, "{}", exitcode)?;
+        // Ignore PIPE errors (in case the front process was
+        // killed by user; the Perl version was simply killed by
+        // SIGPIPE before it had a chance to print the error
+        // message about not being able to print to sigw):
+        let _ = write_all(sigw, &buf);
+        let _ = close(sigw);
+
+        unsafe { _exit(0) };
     }
-
-    Ok(())
 }

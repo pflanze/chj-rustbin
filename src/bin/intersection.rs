f6c550d798f5ce9f2e5afd67f479a349b4d6cfd3
Christian Jaeger <ch@christianjaeger.ch>
2023-10-02 23:30:08 +0200

    Move read utils to readwithcontext.rs

--- src/bin/intersection.rs
+++ src/bin/intersection.rs
@@ -1,7 +1,11 @@
 #[path = "../moreordering.rs"]
 mod moreordering;
-#[allow(unused_imports)] // only used with older compiler
+#[allow(unused_imports)] // polyfill only used with older compiler
 use moreordering::MoreOrdering;
+
+#[path = "../readwithcontext.rs"]
+mod readwithcontext;
+use readwithcontext::{easy_read_line, open_file, ReadWithContext};
 
 use anyhow::{Result, bail, Context, Error, anyhow};
 use structopt::StructOpt;
@@ -9,9 +13,9 @@ use thiserror::Error;
 use std::cmp::Ordering;
 use std::fs::File;
 use std::i64::MIN;
-use std::io::{BufReader, BufRead, stdout, Write, BufWriter};
+use std::io::{BufReader, stdout, Write, BufWriter};
 use std::os::unix::prelude::{MetadataExt, FromRawFd};
-use std::path::{PathBuf, Path};
+use std::path::PathBuf;
 use std::collections::{VecDeque, HashSet};
 use kstring::KString;
 
@@ -61,54 +65,6 @@ fn println(out: &mut impl Write, line: &String) -> Result<()> {
     out.write_all(b"\n")?;
     Ok(())
 }
-
-fn trim(line: &mut String) {
-    if line.ends_with("\n") {
-        line.pop().unwrap();
-    }
-}
-
-fn open_file(path: &Path) -> Result<BufReader<File>> {
-    Ok(BufReader::new(File::open(path).with_context(
-        || format!("opening file {:?}", path))?))
-}
-
-// "Clean" read_line function: returns true if it did read a line,
-// false on EOF. Does overwrite `line`, not append to it. Removes
-// trailing '\n' if present.
-fn easy_read_line(inp: &mut BufReader<File>, line: &mut String) -> Result<bool> {
-    line.clear();
-    if inp.read_line(line)? != 0 {
-        trim(line);
-        Ok(true)
-    } else {
-        Ok(false)
-    }
-}
-
-// Automatically count lines and report them and the path in error
-// messages
-struct ReadWithContext<'p> {
-    path: &'p Path,
-    linenumber: i64,
-    reader: BufReader<File>,
-}
-
-impl<'p> ReadWithContext<'p> {
-    fn open_path(path: &'p Path) -> Result<ReadWithContext<'p>> {
-        Ok(ReadWithContext {
-            path,
-            linenumber: 0,
-            reader: open_file(path)?
-        })
-    }
-    fn easy_read_line(&mut self, line: &mut String) -> Result<bool> {
-        self.linenumber += 1;
-        easy_read_line(&mut self.reader, line).with_context(
-            || anyhow!("file {:?} line {}", self.path, self.linenumber))
-    }
-}
-
 
 #[derive(Debug, Clone, Copy)]
 enum SortOrder {
---
+++ src/readwithcontext.rs
@@ -0,0 +1,57 @@
+//! File handling utilities that make life simpler for the common
+//! case.
+
+use std::{path::Path, fs::File, io::{BufReader, BufRead}};
+use anyhow::{anyhow, Result, Context};
+
+
+pub fn trim(line: &mut String) {
+    if line.ends_with("\n") {
+        line.pop().unwrap();
+    }
+}
+
+/// Easy buffered file opening with context on open errors. But see
+/// `ReadWithContext` instead for keeping context across subsequent
+/// calls, too.
+pub fn open_file(path: &Path) -> Result<BufReader<File>> {
+    Ok(BufReader::new(File::open(path).with_context(
+        || format!("opening file {:?}", path))?))
+}
+
+/// "Clean" read_line function: returns true if it did read a line,
+/// false on EOF. Does overwrite `line`, not append to it. Removes
+/// trailing '\n' if present.
+pub fn easy_read_line(inp: &mut BufReader<File>, line: &mut String) -> Result<bool> {
+    line.clear();
+    if inp.read_line(line)? != 0 {
+        trim(line);
+        Ok(true)
+    } else {
+        Ok(false)
+    }
+}
+
+/// Automatically count lines and report them and the path in error
+/// messages
+pub struct ReadWithContext<'p> {
+    path: &'p Path,
+    linenumber: i64,
+    reader: BufReader<File>,
+}
+
+impl<'p> ReadWithContext<'p> {
+    pub fn open_path(path: &'p Path) -> Result<ReadWithContext<'p>> {
+        Ok(ReadWithContext {
+            path,
+            linenumber: 0,
+            reader: open_file(path)?
+        })
+    }
+    pub fn easy_read_line(&mut self, line: &mut String) -> Result<bool> {
+        self.linenumber += 1;
+        easy_read_line(&mut self.reader, line).with_context(
+            || anyhow!("file {:?} line {}", self.path, self.linenumber))
+    }
+}
+

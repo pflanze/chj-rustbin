aced96320e3f64f4ada5491573da2d77cddc2cff
Christian Jaeger <ch@christianjaeger.ch>
2026-02-07 07:54:11 +0100

    util/scope.rs: note about Rust 2024

--- src/util/scope.rs
+++ src/util/scope.rs
@@ -1,6 +1,8 @@
 /// Make a delimited for temporary objects. Unlike `{ ... }` which is
 /// not enough, this macro also includes a `;` which does the
 /// trick. Useful e.g. to limit the scope of Mutex or RefCell guards.
+/// -- Ah: note that with Rust edition 2024 this will be useless, as
+/// there the `{ ... }` already does limit the life time scope!
 #[macro_export]
 macro_rules! scope {
     { $($code:tt)* } => {{

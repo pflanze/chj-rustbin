3d33c1a3eeb3d48f3a8e01bb9beed7c0b0451b4d
Christian Jaeger <ch@christianjaeger.ch>
2026-03-03 19:51:49 +0100

    lrt: add --reverse, and --skip, --head, --tail, --reverse-after options

--- src/bin/lrt.rs
+++ src/bin/lrt.rs
@@ -21,6 +21,26 @@ struct Opt {
     /// Print output records separated by zero-bytes
     #[clap(long)]
     zo: bool,
+
+    /// Sort in the reverse, show newest items first
+    #[clap(long)]
+    reverse: bool,
+
+    /// Skip up to n items from the top
+    #[clap(long)]
+    skip: Option<usize>,
+
+    /// Print only up to the n bottom items (after skip)
+    #[clap(long)]
+    head: Option<usize>,
+
+    /// Print only up to the n bottom items (after head)
+    #[clap(long)]
+    tail: Option<usize>,
+
+    /// Reverse after processing skip, head and tail
+    #[clap(long)]
+    reverse_after: bool,
 }
 
 fn chomp(v: &mut Vec<u8>, record_separator: u8) {
@@ -37,7 +57,15 @@ struct Item<'t> {
 }
 
 fn main() -> Result<()> {
-    let Opt { z, zo } = Opt::from_args();
+    let Opt {
+        z,
+        zo,
+        reverse,
+        skip,
+        head,
+        tail,
+        reverse_after,
+    } = Opt::from_args();
     let input_record_separator = if z { 0 } else { b'\n' };
     let output_record_separator = if zo { 0 } else { b'\n' };
 
@@ -66,11 +94,40 @@ fn main() -> Result<()> {
         })
         .filter_map(|r| r.transpose())
         .collect::<Result<Vec<Item>>>()?;
-    items.sort_by(|a, b| a.mtime.cmp(&b.mtime));
+
+    if reverse {
+        items.sort_by(|b, a| {
+            a.mtime.cmp(&b.mtime).then_with(|| a.path.cmp(&b.path))
+        });
+    } else {
+        items.sort_by(|a, b| {
+            a.mtime.cmp(&b.mtime).then_with(|| a.path.cmp(&b.path))
+        });
+    }
+
+    let mut selected_items = &mut *items;
+    if let Some(skip) = skip {
+        let n = selected_items.len().min(skip);
+        selected_items = &mut selected_items[n..]
+    }
+    if let Some(head) = head {
+        let n = selected_items.len().min(head);
+        selected_items = &mut selected_items[..n]
+    }
+    if let Some(tail) = tail {
+        let n = selected_items.len().saturating_sub(tail);
+        selected_items = &mut selected_items[n..]
+    }
+
+    #[allow(unused)]
+    let items = ();
+    if reverse_after {
+        selected_items.reverse()
+    }
 
     let mut outp = BufWriter::new(stdout().lock());
     (|| -> Result<()> {
-        for item in items {
+        for item in selected_items {
             outp.write_all(item.path.as_os_str().as_bytes())?;
             outp.write_all(&[output_record_separator])?;
         }

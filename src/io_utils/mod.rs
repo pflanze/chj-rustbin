58e545060b6e07117ed8159faab779b3c9ed4c27
Christian Jaeger <ch@christianjaeger.ch>
2026-03-10 00:44:10 +0100

    Add io_utils/read_dir_bufs.rs

--- src/io_utils/mod.rs
+++ src/io_utils/mod.rs
@@ -1,1 +1,2 @@
 pub mod read_buf;
+pub mod read_dir_bufs;
---
+++ src/io_utils/read_dir_bufs.rs
@@ -0,0 +1,71 @@
+//! Almost-copy-paste of `read_buf.rs`
+
+use std::{fs::ReadDir, os::unix::ffi::OsStrExt};
+
+use rayon::iter::{plumbing::Folder, ParallelIterator};
+
+use crate::io_utils::read_buf::ReadBufStreamError;
+
+pub struct ReadDirBufStream {
+    input: ReadDir,
+    buf_size: usize,
+    record_separator: u8,
+    buf: Vec<u8>,
+}
+
+impl ReadDirBufStream {
+    /// Unlike for `ReadBufStream` where `buf_size` is the maximum
+    /// buffer size, here it is the minimal: paths are added to a
+    /// buffer until it is at least the given size.
+    pub fn new(input: ReadDir, buf_size: usize, record_separator: u8) -> Self {
+        Self {
+            input,
+            buf_size,
+            record_separator,
+            buf: Vec::new(),
+        }
+    }
+}
+
+impl Iterator for &mut ReadDirBufStream {
+    /// Use `ReadBufStreamError` to stay compatible with `ParReadBufStream`
+    type Item = Result<Vec<u8>, ReadBufStreamError>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        (|| -> Result<Option<Vec<u8>>, ReadBufStreamError> {
+            while self.buf.len() < self.buf_size {
+                if let Some(item) = self.input.next().transpose()? {
+                    // XX parameterize whether file_name or path
+                    let filename = item.file_name();
+                    self.buf.extend_from_slice(filename.as_bytes());
+                    self.buf.push(self.record_separator);
+                } else {
+                    break;
+                }
+            }
+
+            if self.buf.is_empty() {
+                Ok(None)
+            } else {
+                let mut new_buf = Vec::with_capacity(self.buf_size + 200);
+                std::mem::swap(&mut self.buf, &mut new_buf);
+                Ok(Some(new_buf))
+            }
+        })()
+        .transpose()
+    }
+}
+
+impl ParallelIterator for ReadDirBufStream {
+    type Item = Result<Vec<u8>, ReadBufStreamError>;
+
+    fn drive_unindexed<C>(mut self, consumer: C) -> C::Result
+    where
+        C: rayon::iter::plumbing::UnindexedConsumer<Self::Item>,
+    {
+        let folder = consumer.into_folder();
+        // XX surely evil, held across whole duration, or what?
+        // Disabling concurrency?
+        folder.consume_iter(&mut self).complete()
+    }
+}

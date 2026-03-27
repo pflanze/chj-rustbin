7c13eb73d710447b717a06fb9e84d62c92d8f886
Christian Jaeger <ch@christianjaeger.ch>
2026-03-27 18:52:38 +0100

    bin/test-leaked_region.rs: check silently
    This makes it run much faster in Miri, since it avoids the slow Debug
    printing (from `dbg!(pss)`). Additionally it checks all values again
    automatically, which avoids the risk for missing corruption.

--- src/bin/test-leaked_region.rs
+++ src/bin/test-leaked_region.rs
@@ -5,39 +5,37 @@ use chj_rustbin::leaked_region::GlobalLeakedRegions;
 
 fn main() -> Result<()> {
     let regions = &GlobalLeakedRegions::new(1000);
-    let pss = &Mutex::new(Vec::new());
-    thread::scope(|scope| {
-        for i in 0..2 {
-            scope.spawn(move || {
-                let path = format!("foo bar banm bum {i}");
-                let path: &Path = path.as_ref();
-                let mut region = regions.get_region();
-                let mut ps = Vec::new();
-                // 150*(18+1) = 2850, meaning 3 regions used
-                for _ in 0..150 {
-                    let p = region.allocate_path(path);
-                    assert_eq!(p, path);
-                    ps.push(p);
-                }
-                pss.lock().expect("no panics").push(ps);
-                eprintln!("finished {i}");
-            });
+    let pss = Mutex::new(Vec::new());
+    {
+        let pss = &pss;
+        thread::scope(|scope| {
+            for i in 0..2 {
+                scope.spawn(move || {
+                    let path = format!("foo bar banm bum {i}");
+                    let path: &Path = path.as_ref();
+                    let mut region = regions.get_region();
+                    let mut ps = Vec::new();
+                    // 150*(18+1) = 2850, meaning 3 regions used
+                    for _ in 0..150 {
+                        let p = region.allocate_path(path);
+                        assert_eq!(p, path);
+                        ps.push(p);
+                    }
+                    pss.lock().expect("no panics").push((i, ps));
+                    eprintln!("finished {i}");
+                });
+            }
+        });
+    }
+    let pss = pss.into_inner().expect("no panics");
+    assert_eq!(pss.len(), 2);
+    for (i, ps) in pss {
+        let path = format!("foo bar banm bum {i}");
+        let path: &Path = path.as_ref();
+        for p in ps {
+            assert_eq!(p, path);
         }
-    });
-    dbg!(pss);
-    // thread::scope(|scope| {
-    //     for i in 2..4 {
-    //         scope.spawn(move || {
-    //             let path = format!("foo bar banm bum {i}");
-    //             let path: &Path = path.as_ref();
-    //             let mut region = regions_rf.get_region();
-    //             for _ in 0..1000 {
-    //                 let p = region.allocate_path(path);
-    //                 assert_eq!(p, path);
-    //             }
-    //             eprintln!("finished {i}");
-    //         });
-    //     }
-    // });
+    }
+
     Ok(())
 }

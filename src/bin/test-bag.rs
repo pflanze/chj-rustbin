49cb7e36813539b08b3db2c332b032b9ebc536f7
Christian Jaeger <ch@christianjaeger.ch>
2026-04-11 19:00:43 +0200

    test-bag: add probes

--- src/bin/test-bag.rs
+++ src/bin/test-bag.rs
@@ -1,13 +1,25 @@
 use anyhow::Result;
 use arbitrary::{Arbitrary, Unstructured};
-use chj_rustbin::bag::Bag;
+use chj_rustbin::{bag::Bag, cpu_probe, probe};
 use getrandom::getrandom;
 
 fn main() -> Result<()> {
-    let mut random_data = vec![0; 10_000_000];
-    getrandom(&mut random_data)?;
-    let mut data = Unstructured::new(&random_data);
-    let bag = Bag::<i32>::arbitrary(&mut data)?;
+    cpu_probe::init()?;
+
+    let random_data = {
+        probe!("random_data");
+        let mut random_data = vec![0; 10_000_000];
+        getrandom(&mut random_data)?;
+        random_data
+    };
+    let mut data = {
+        probe!("data");
+        Unstructured::new(&random_data)
+    };
+    let bag = {
+        probe!("arbitrary");
+        Bag::<i32>::arbitrary(&mut data)?
+    };
 
     let show_it = false;
     if show_it {
@@ -15,8 +27,18 @@ fn main() -> Result<()> {
     }
     eprintln!("bag.len() = {}", bag.len());
 
-    let l1 = bag.clone().flatten();
-    let l2 = bag.par_flatten();
+    let l1 = {
+        let bag = {
+            probe!("clone");
+            bag.clone()
+        };
+        probe!("flatten");
+        bag.flatten()
+    };
+    let l2 = {
+        probe!("par_flatten");
+        bag.par_flatten()
+    };
     assert_eq!(l1, l2);
 
     if show_it {

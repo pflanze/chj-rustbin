b5499fc26c9146e541a373c9b6ba40ac3e050af3
Christian Jaeger <ch@christianjaeger.ch>
2026-04-12 16:17:16 +0200

    unsync_unsend.rs: clean up docs

--- src/unsync_unsend.rs
+++ src/unsync_unsend.rs
@@ -2,21 +2,36 @@
 //!
 //! If you want !Send, you need to add a marker field to your struct:
 //!
+//! ```
+//! use std::marker::PhantomData;
+//! use chj_rustbin::unsync_unsend::UnSend;
+//!
 //! struct Foo {
 //!     _marker: PhantomData<UnSend>,
 //! }
+//! ```
 //!
 //! For !Sync,
+//!
+//! ```
+//! use std::marker::PhantomData;
+//! use chj_rustbin::unsync_unsend::UnSync;
 //!
 //! struct Foo {
 //!     _marker: PhantomData<UnSync>,
 //! }
+//! ```
 //!
 //! To disable both at the same time,
+//!
+//! ```
+//! use std::marker::PhantomData;
+//! use chj_rustbin::unsync_unsend::UnSyncUnSend;
 //!
 //! struct Foo {
 //!     _marker: PhantomData<UnSyncUnSend>,
 //! }
+//! ```
 
 // https://stackoverflow.com/questions/62713667/how-to-implement-send-or-sync-for-a-type
 

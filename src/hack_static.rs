8f138a0a2382eccd0ff27c07a68f458d917b4e95
Christian Jaeger <ch@christianjaeger.ch>
2026-04-27 02:30:21 +0200

    hack_static.rs: add `hack_static_const`
    Unused, though.

--- src/hack_static.rs
+++ src/hack_static.rs
@@ -4,3 +4,8 @@ use std::mem::transmute;
 pub unsafe fn hack_static<'a, 'v, T: ?Sized>(rf: &'a mut T) -> &'v mut T {
     transmute(rf)
 }
+
+#[allow(clippy::needless_lifetimes)]
+pub unsafe fn hack_static_const<'a, 'v, T: ?Sized>(rf: &'a T) -> &'v T {
+    transmute(rf)
+}

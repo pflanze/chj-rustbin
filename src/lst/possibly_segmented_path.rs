d9febf6bf056c1ae5ccbd83328ace0bdb298fae0
Christian Jaeger <ch@christianjaeger.ch>
2026-04-28 04:18:23 +0200

    lst/possibly_segmented_path.rs optimize: avoid heap allocation(s)
    Makes a clear, but not large, difference, in the "get items" cost.

--- src/lst/possibly_segmented_path.rs
+++ src/lst/possibly_segmented_path.rs
@@ -39,10 +39,27 @@ impl<'region, INLINE> PossiblySegmentedPath<'region, INLINE> for &'region Path {
         file_name: &OsStr,
         region: &mut LeakedRegion<'region>,
     ) -> Self {
-        // XX optimize: copy directly to region! Will see the cost now.
-        let mut p = self.to_owned();
-        p.push(file_name);
-        region.allocate_path(&p)
+        if false {
+            let mut p = self.to_owned();
+            p.push(file_name);
+            region.allocate_path(&p)
+        } else {
+            // optimize: copy directly to region
+            let b0 = self.as_os_str().as_encoded_bytes();
+            let b1 = file_name.as_encoded_bytes();
+            let l0 = b0.len();
+            let l1 = b1.len();
+            let len = l0 + 1 + l1;
+            let alloc = region.allocate::<1>(len);
+            alloc[0..l0].copy_from_slice(b0);
+            alloc[l0] = b'/';
+            alloc[l0 + 1..len].copy_from_slice(b1);
+            unsafe {
+                // Safety: should be OK?
+                OsStr::from_encoded_bytes_unchecked(alloc)
+            }
+            .as_ref()
+        }
     }
 
     fn psp_to_path<'tmp>(self, _tmp: &'tmp mut Vec<u8>) -> &'tmp Path

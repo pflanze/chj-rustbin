0697df4b089db162cd4f89979b50a8a44b53b9f5
Christian Jaeger <ch@christianjaeger.ch>
2026-05-02 07:06:04 +0200

    Split off and merge with other UnixFileType

--- src/io/unix/unix_file_type.rs
+++ src/io/unix/unix_file_type.rs
@@ -1,6 +1,7 @@
+use anyhow::{bail, Result};
 use enumn::N;
 
-#[derive(N, Eq, PartialEq, Debug)]
+#[derive(N, Clone, Copy, Debug, PartialEq, Eq)]
 #[repr(u8)]
 pub enum UnixFileType {
     // XX are these Linux-specific, use C constants?
@@ -11,4 +12,60 @@ pub enum UnixFileType {
     File = 8,
     Link = 10,
     Socket = 12,
+    // Linux uses this value when getting `metadata` for
+    // /proc/$pid/task/$tid/fd/$n symlink
+    None = 0,
+}
+
+impl UnixFileType {
+    pub fn is_dir(self) -> bool {
+        self == UnixFileType::Dir
+    }
+    pub fn is_file(self) -> bool {
+        self == UnixFileType::File
+    }
+    pub fn is_link(self) -> bool {
+        self == UnixFileType::Link
+    }
+    /// Char as used by the `ls` command with `-l`
+    pub fn type_char(self) -> char {
+        match self {
+            UnixFileType::File => '-',
+            UnixFileType::Dir => 'd',
+            UnixFileType::Link => 'l',
+            UnixFileType::Socket => 's',
+            UnixFileType::CharDevice => 'c',
+            UnixFileType::BlockDevice => 'b',
+            UnixFileType::Pipe => 'p',
+            UnixFileType::None => 'N',
+        }
+    }
+    pub fn has_device_info(self) -> bool {
+        match self {
+            UnixFileType::File
+            | UnixFileType::Dir
+            | UnixFileType::Link
+            | UnixFileType::Socket
+            | UnixFileType::Pipe
+            | UnixFileType::None => false,
+            UnixFileType::CharDevice | UnixFileType::BlockDevice => true,
+        }
+    }
+}
+
+impl TryFrom<u8> for UnixFileType {
+    type Error = anyhow::Error;
+    fn try_from(m: u8) -> Result<Self> {
+        match m {
+            8 => Ok(UnixFileType::File),
+            4 => Ok(UnixFileType::Dir),
+            10 => Ok(UnixFileType::Link),
+            12 => Ok(UnixFileType::Socket),
+            2 => Ok(UnixFileType::CharDevice),
+            6 => Ok(UnixFileType::BlockDevice),
+            1 => Ok(UnixFileType::Pipe),
+            0 => Ok(UnixFileType::None),
+            _ => bail!("invalid file type number {m}"),
+        }
+    }
 }
--- src/lst/get_items.rs
+++ src/lst/get_items.rs
@@ -13,6 +13,7 @@ use anstyle::{AnsiColor, Color, Style};
 use anyhow::{anyhow, bail, Context, Result};
 use rayon::iter::ParallelIterator;
 
+pub use crate::io::unix::unix_file_type::UnixFileType;
 use crate::{
     bag::Bag,
     efficient_regex::EfficientRegex,
@@ -37,73 +38,6 @@ fn chomp(v: &mut Vec<u8>, record_separator: u8) {
 }
 
 const MIN_OUT_SLICE_LEN: usize = 500000;
-
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
-pub enum UnixFileType {
-    File = 8,
-    Dir = 4,
-    Link = 10,
-    Socket = 12,
-    CharDevice = 2,
-    BlockDevice = 6,
-    Pipe = 1,
-    // Linux uses this value when getting `metadata` for
-    // /proc/$pid/task/$tid/fd/$n symlink
-    None = 0,
-}
-
-impl UnixFileType {
-    pub fn is_dir(self) -> bool {
-        self == UnixFileType::Dir
-    }
-    pub fn is_file(self) -> bool {
-        self == UnixFileType::File
-    }
-    pub fn is_link(self) -> bool {
-        self == UnixFileType::Link
-    }
-    /// Char as used by the `ls` command with `-l`
-    pub fn type_char(self) -> char {
-        match self {
-            UnixFileType::File => '-',
-            UnixFileType::Dir => 'd',
-            UnixFileType::Link => 'l',
-            UnixFileType::Socket => 's',
-            UnixFileType::CharDevice => 'c',
-            UnixFileType::BlockDevice => 'b',
-            UnixFileType::Pipe => 'p',
-            UnixFileType::None => 'N',
-        }
-    }
-    pub fn has_device_info(self) -> bool {
-        match self {
-            UnixFileType::File
-            | UnixFileType::Dir
-            | UnixFileType::Link
-            | UnixFileType::Socket
-            | UnixFileType::Pipe
-            | UnixFileType::None => false,
-            UnixFileType::CharDevice | UnixFileType::BlockDevice => true,
-        }
-    }
-}
-
-impl TryFrom<u8> for UnixFileType {
-    type Error = anyhow::Error;
-    fn try_from(m: u8) -> Result<Self> {
-        match m {
-            8 => Ok(UnixFileType::File),
-            4 => Ok(UnixFileType::Dir),
-            10 => Ok(UnixFileType::Link),
-            12 => Ok(UnixFileType::Socket),
-            2 => Ok(UnixFileType::CharDevice),
-            6 => Ok(UnixFileType::BlockDevice),
-            1 => Ok(UnixFileType::Pipe),
-            0 => Ok(UnixFileType::None),
-            _ => bail!("invalid file type number {m}"),
-        }
-    }
-}
 
 pub trait RwxPosition {
     const S_CHAR_SET: char;

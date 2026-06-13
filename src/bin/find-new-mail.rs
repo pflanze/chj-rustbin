dd49859db96f11693ac0baafd66eb86581b4465b
Christian Jaeger <ch@christianjaeger.ch>
2026-06-13 19:01:57 +0200

    Create find-new-mail

---
+++ src/bin/find-new-mail.rs
@@ -0,0 +1,145 @@
+use std::{
+    path::{Path, PathBuf},
+    str::FromStr,
+    time::{Duration, SystemTime},
+};
+
+use chj_rustbin::mystring::MyString;
+use clap::Parser;
+
+use anyhow::{anyhow, bail, Context, Result};
+
+#[derive(clap::Parser, Debug)]
+struct FilterOpts {
+    /// Mails must carry a delivery time newer than this unix time
+    /// stamp
+    #[clap(long)]
+    newer_than_unixtime: Option<u64>,
+
+    /// Mails must carry a delivery time newer than the modification
+    /// time of the file at this path
+    #[clap(long)]
+    newer_than_file_path: Option<PathBuf>,
+}
+
+impl FilterOpts {
+    /// 'Parse' the given options into a single time point; returns
+    /// None if no option was given. Returns an error if a given
+    /// file's path couldn't be used.
+    fn newer_than_unixtime(&self) -> Result<Option<u64>> {
+        match self {
+            FilterOpts {
+                newer_than_unixtime: Some(t),
+                newer_than_file_path: None,
+            } => Ok(Some(*t)),
+            FilterOpts {
+                newer_than_unixtime: None,
+                newer_than_file_path: Some(path),
+            } => (|| -> Result<_> {
+                let m: std::fs::Metadata = path.metadata()?;
+                let mtime: SystemTime = m.modified()?;
+                let unix: u64 =
+                    mtime.duration_since(SystemTime::UNIX_EPOCH)?.as_secs();
+                Ok(Some(unix))
+            })()
+            .with_context(|| {
+                anyhow!("getting modification time of path {path:?}")
+            }),
+            FilterOpts {
+                newer_than_unixtime: Some(_),
+                newer_than_file_path: Some(_),
+            } => {
+                bail!(
+                    "both the --newer-than-unixtime and \
+                     --newer-than-file-path options were given"
+                )
+            }
+            FilterOpts {
+                newer_than_unixtime: None,
+                newer_than_file_path: None,
+            } => Ok(None),
+        }
+    }
+}
+
+#[derive(clap::Parser, Debug)]
+/// Print files in a Maildir that have a newer delivery time than a
+/// given time.
+#[clap(name = "find-new-mail from chj-rustbin")]
+struct Opt {
+    /// Poll until there are files to be listed
+    #[clap(long)]
+    poll: bool,
+
+    /// How long to sleep between polls in seconds (float)
+    #[clap(long, default_value = "3")]
+    sleep_time: f64,
+
+    #[clap(flatten)]
+    filter_opts: FilterOpts,
+
+    /// Path to the directory with the incoming email
+    dir: PathBuf,
+}
+
+fn check(
+    dir: &Path,
+    filter_opts: &FilterOpts,
+) -> Result<Vec<(u64, MyString<23>)>> {
+    let newer_than_time = filter_opts.newer_than_unixtime()?;
+    let dir = std::fs::read_dir(&dir)?;
+    let mut items = Vec::new();
+    for item in dir {
+        let item = item?;
+        let name = item.file_name();
+        if let Ok(name) = name.into_string() {
+            // 1622820231.16542.foo
+            if let Some((timestr, _)) = name.split_once('.') {
+                if let Ok(time) = u64::from_str(timestr) {
+                    if let Some(min_time) = newer_than_time {
+                        if time > min_time {
+                            items.push((time, name.into()));
+                        }
+                    } else {
+                        // Always print
+                        items.push((time, name.into()));
+                    }
+                } else {
+                    // warn?
+                }
+            } else {
+                // warn?
+            }
+        } else {
+            // warn?
+        }
+    }
+    Ok(items)
+}
+
+fn main() -> Result<()> {
+    let opt: Opt = Opt::parse();
+
+    let mut found = if opt.poll {
+        (|| -> Result<Vec<_>> {
+            loop {
+                let found = check(&opt.dir, &opt.filter_opts)?;
+                if !found.is_empty() {
+                    return Ok(found);
+                }
+                std::thread::sleep(Duration::from_secs_f64(opt.sleep_time));
+            }
+        })()
+    } else {
+        check(&opt.dir, &opt.filter_opts)
+    }
+    .with_context(|| anyhow!("reading directory {:?}", opt.dir))?;
+
+    found.sort();
+
+    for item in found {
+        println!("{}", item.1);
+    }
+
+    Ok(())
+}
